6b3be956a89e947aeed6dbba97e2f0574676f34f
oezcan <Fikriye.Oezcan@student.uibk.ac.at>
2020-04-21 22:06:13 +0200

    ex3

--- 04/ex_3/ex3_parallel.c
+++ 04/ex_3/ex3_parallel.c
@@ -1,14 +1,12 @@
-// mergesort-sections.c
 #pragma omp parallel sections
 {
-#pragma omp section
-{
-mergesort_parallel_omp (a, size / 2, temp, threads / 2);
-}
-#pragma omp section
-{
-mergesort_parallel_omp (a + size / 2, size - size / 2,
-temp + size / 2, threads - threads / 2);
-}
+    #pragma omp section
+    {
+        mergesort_parallel_omp (a, size / 2, temp, threads / 2);
+    }
+    #pragma omp section
+    {
+        mergesort_parallel_omp (a + size / 2, size - size / 2, temp + size / 2, threads - threads / 2);
+    }
 }
 merge(a, size, temp);
