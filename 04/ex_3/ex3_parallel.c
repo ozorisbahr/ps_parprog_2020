bac03cc25898f0bd08814f42b50741fd3075bcfd
oezcan <Fikriye.Oezcan@student.uibk.ac.at>
2020-04-28 18:15:23 +0200

    job flush

--- 04/ex_3/ex3_parallel.c
+++ 04/ex_3/ex3_parallel.c
@@ -1,12 +1,30 @@
-#pragma omp parallel sections
-{
-    #pragma omp section
-    {
-        mergesort_parallel_omp (a, size / 2, temp, threads / 2);
-    }
-    #pragma omp section
-    {
-        mergesort_parallel_omp (a + size / 2, size - size / 2, temp + size / 2, threads - threads / 2);
+template <int T> void P_Merge(T *C, T *A, T *B, int na, int nb) {
+    if (na < nb) {
+        P_Merge(C, B, A, nb, na);
+    } else if (na==0) { return;
+    } else {
+        int ma = na/2;
+        int mb = BinarySearch(A[ma], B, nb);
+        C[ma+mb] = A[ma];
+        #pragma omp task
+            P_Merge(C, A, B, ma, mb);
+        #pragma omp task
+         P_Merge(C+ma+mb+1,A+ma+1,B+mb,na-ma-1,nb-mb);
+         #pragma omp taskwait;
     }
 }
-merge(a, size, temp);
+    template <int T>
+void P_MergeSort(T *B, T *A, int n) {
+    if (n==1) {
+        B[0] = A[0];
+    } else {
+        T C[n];
+        #pragma omp task
+            P_MergeSort(C,A,n/2);
+        #pragma omp task
+            P_MergeSort(C+n/2,A+n/2,n-n/2);
+        #pragma omp taskwait;
+        P_Merge(B,C,C+n/2, n/2, n-n/2);
+
+    }
+}
---
+++ 05/job_task2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Executes job in the queue "std.q" unless you have special requirements.
+#$ -q std.q
+
+# Changes to the current working directory before performing any further action
+#$ -cwd
+
+# Name of your job. Unless you use the -o and -e options, output will
+# go to a unique file name.ojob_id for each job.
+#$ -N flush
+
+# Redirect output stream to this file.
+#$ -o output.dat
+
+# Join the error stream to the output stream.
+#$ -j yes
+
+# Parallel environment for using OpenMP, allocates 8 cores on a single node
+#$ -pe openmp 8
+
+# Alocate 4 GB memory for each core
+#$ -l h_vmem=4G
+
+# Use gcc 8.2.0 as the default gcc
+module load gcc/8.2.0
+
+# Set up any environment variables
+./flush
+
