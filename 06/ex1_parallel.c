b160a39c0a997f7715f96391ca1170b153b5f2cf
portscher <62957205+portscher@users.noreply.github.com>
2020-05-03 18:57:45 +0200

    Update ex1_parallel.c

--- 06/ex1_parallel.c
+++ 06/ex1_parallel.c
@@ -30,37 +30,49 @@ int calculate_folder_size(const char *path) {
     struct dirent *element;
     size_t size = 4096;
 
-    // iterate through all elements in the folder, until element = NULL
-    // readdir always returns the next element in the folder
+
     for (element = readdir(folder); element != NULL; element = readdir(folder)) {
 
-        long path_length = sizeof(char) * (strlen(path) + strlen(element->d_name) + 2);
-        char *name = (char *) malloc(path_length);
+        {
+            long path_length = sizeof(char) * (strlen(path) + strlen(element->d_name) + 2);
+            char *name = (char *) malloc(path_length);
+            strcpy(name, path);
+            strcpy(name + strlen(path), "/");
+            strcpy(name + strlen(path) + 1, element->d_name);
 
-        strcpy(name, path);
-        strcpy(name + strlen(path), "/");
-        strcpy(name + strlen(path) + 1, element->d_name);
+            if (element->d_type == DT_DIR) {
+                if (strcmp(element->d_name, ".") != 0 && strcmp(element->d_name, "..") != 0) {
+                    // Copying the path name ensures that the task receives a copy of the name.
+                    // The task needs the path name to exist until it got executed
+                    char *priv_name = strdup(name);
 
-        if (element->d_type == DT_DIR) {
-            if (strcmp(element->d_name, ".") != 0 && strcmp(element->d_name, "..") != 0) {
+                    // The task needs to remember the current value of priv_name as it will change in the next loop
+                    // iteration --> firstprivate. size will need to be modified --> shared.
+#pragma omp task shared(size) firstprivate(priv_name)
+                    {
+                        // as all tasks will write to the variable size, the writing operations should be atomic
+#pragma omp atomic update
+                        // recursive call of calculate_folder_size
+                        size += calculate_folder_size(priv_name);
 
-                // recursive call of calculate_folder_size
-
-                size += calculate_folder_size(name);
-            }
-        } else {
-
-            int status = lstat(name, &sb);
-            if (status == 0) {
-                size += sb.st_size;
+                        // No longer needed.
+                        free(priv_name);
+                    }
+                }
             } else {
-                perror("LSTAT ERROR\n");
+
+                int status = lstat(name, &sb);
+                if (status == 0) {
+                    size += sb.st_size;
+                } else {
+                    perror("LSTAT ERROR\n");
+                }
             }
+            free(name);
         }
-        free(name);
     }
-
-
+// Parent tasks waits for all children to finish before returning the result
+#pragma omp taskwait
     closedir(folder);
 
     return size;
@@ -111,3 +123,6 @@ int main(int argc, char **argv) {
     double end_time = omp_get_wtime();
     printf("Size: %d, Elapsed time: %2.2f seconds\n", folder_size, end_time - start_time);
 }
+
+// Some input from stackoverflow user Hristo Iliev
+// https://stackoverflow.com/users/1374437/hristo-iliev
